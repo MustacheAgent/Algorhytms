e245151feb7f6e3b24a2cc99265de6b0301e2788
MustacheAgent <dimonus98@yandex.ru>
2021-11-01 14:23:27 +0300

    комментарии

--- Transposition/Testing/UnitTest1.cs
+++ Transposition/Testing/UnitTest1.cs
@@ -34,5 +34,49 @@ namespace Testing
 
             Assert.AreEqual(test, res);
         }
+
+        [Test]
+        public void Test2()
+        {
+            int[] res;
+            using (StreamReader reader = new StreamReader(TestContext.CurrentContext.TestDirectory + "\\test2.txt"))
+            {
+                int[] param = Array.ConvertAll(reader.ReadLine().Split(' '), int.Parse); // количество элементов и количество наборов перестановок
+                int[][] swaps = new int[param[1]][]; // массив массивов перестановок
+                for (int i = 0; i < param[1]; i++)
+                {
+                    swaps[i] = Array.ConvertAll(reader.ReadLine().Split(' '), s => int.Parse(s) - 1); // получение перестановок
+                }
+                reader.ReadLine();
+                int[] swapsOrder = Array.ConvertAll(reader.ReadLine().Split(' '), s => int.Parse(s) - 1); // порядок применения перестановок
+                res = Program.Solve(swaps, swapsOrder);
+            }
+
+            int[] test = new int[] { 2, 1, 2 };
+
+            Assert.AreEqual(test, res);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int[] res;
+            using (StreamReader reader = new StreamReader(TestContext.CurrentContext.TestDirectory + "\\test3.txt"))
+            {
+                int[] param = Array.ConvertAll(reader.ReadLine().Split(' '), int.Parse); // количество элементов и количество наборов перестановок
+                int[][] swaps = new int[param[1]][]; // массив массивов перестановок
+                for (int i = 0; i < param[1]; i++)
+                {
+                    swaps[i] = Array.ConvertAll(reader.ReadLine().Split(' '), s => int.Parse(s) - 1); // получение перестановок
+                }
+                reader.ReadLine();
+                int[] swapsOrder = Array.ConvertAll(reader.ReadLine().Split(' '), s => int.Parse(s) - 1); // порядок применения перестановок
+                res = Program.Solve(swaps, swapsOrder);
+            }
+
+            int[] test = new int[] { 1 };
+
+            Assert.AreEqual(test, res);
+        }
     }
 }
--- Transposition/Transposition/Program.cs
+++ Transposition/Transposition/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Transposition
 {
@@ -6,6 +7,7 @@ namespace Transposition
     {
         static void Main(string[] args)
         {
+            /*
             int[] param = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse); // количество элементов и количество наборов перестановок
             int[][] swaps = new int[param[1]][]; // массив массивов перестановок
             for (int i = 0; i < param[1]; i++)
@@ -14,14 +16,37 @@ namespace Transposition
             }
             Console.ReadLine();
             int[] swapsOrder = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s) - 1); // порядок применения перестановок
-
-            int[] res = Solve(swaps, swapsOrder);
-            foreach (var item in res)
+            */
+            StreamReader reader = new StreamReader("input.txt");
+            StreamWriter writer = new StreamWriter("output.txt");
+            int[] param = Array.ConvertAll(reader.ReadLine().Split(' '), int.Parse); // количество элементов и количество наборов перестановок
+            int[][] swaps = new int[param[1]][]; // массив массивов перестановок
+            for (int i = 0; i < param[1]; i++)
             {
-                Console.WriteLine((item + 1).ToString());
+                swaps[i] = Array.ConvertAll(reader.ReadLine().Split(' '), s => int.Parse(s) - 1); // получение перестановок
+            }
+            reader.ReadLine();
+            int[] swapsOrder = Array.ConvertAll(reader.ReadLine().Split(' '), s => int.Parse(s) - 1); // порядок применения перестановок
+
+            int[] result = new int[swapsOrder.Length]; // массив ответов (позиций первого элемента)
+
+            for (int i = 0; i < swapsOrder.Length; i++) // исследуем каждую перестановку
+            {
+                for (int j = 0; j < swapsOrder.Length; j++) // применяем все перестановки кроме выбранной
+                {
+                    if (j == i) continue; // собственно пропускаем выбранную перестановку, если натыкаемся
+
+                    // магия: смотрим на индекс первого элемента в первой применяемой перестановке, запоминаем его новую позицию,
+                    // и в следующей перестановке проверяем уже запомненную позицию, получая следующую позицию, и так далее, пока не переберем все перестановки
+                    result[i] = swaps[swapsOrder[j]][result[i]]; 
+                }
+                result[i]++; // массив нумеруется с нуля же
             }
 
-            Console.ReadKey();
+            foreach (var item in result)
+            {
+                writer.WriteLine((item).ToString()); // вывод в файл
+            }
         }
 
         public static int[] Solve(int[][] swaps, int[] swapsOrder)
@@ -35,6 +60,7 @@ namespace Transposition
                     if (j == i) continue;
                     result[i] = swaps[swapsOrder[j]][result[i]];
                 }
+                result[i]++;
             }
             return result;
         }
